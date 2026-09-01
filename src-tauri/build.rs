32cde15915c807c96ed07d22a7e4e713e69daf44
sajjad isvand <sajjadzibaee102@gmail.com>
2026-09-01 17:43:52 +0330

    feat: require administrator privileges via win32 application manifest

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    tauri_build::build()
+    let mut windows = tauri_build::WindowsAttributes::new();
+    windows = windows.app_manifest(include_str!("admin.manifest"));
+    let attrs = tauri_build::Attributes::new().windows_attributes(windows);
+    tauri_build::try_build(attrs).expect("failed to run build script");
 }
